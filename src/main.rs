eeb616d72edc6dedf0b5c5511fdeb56f267821e1
Gabriel Saiago <grsaiago@gmail.com>
2024-09-15 00:23:58 -0300

    Add: minimal axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/ping", get(|| async { "Pong" }));
+    let listener = TcpListener::bind("127.0.0.1:8080")
+        .await
+        .expect("Bind failed");
+    let _ = axum::serve(listener, app).await;
 }
